6e24a8b12f312a73069135e777bbddd30f524bf7
Sabrina Li <119278456+5a6r1na@users.noreply.github.com>
2026-09-10 13:22:18 -0500

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        track = 0
+
+        for num in nums:
+            track ^= num
+        
+        return track
